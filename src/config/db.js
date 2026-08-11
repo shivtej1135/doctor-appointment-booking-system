483a4a334cceecc9056d03a8c1cb8753f1089d01
Shivtej Ghorpade <is23bm033@iitdh.ac.in>
2026-08-12 00:27:22 +0530

    Enable SSL for Neon database

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const pool=new Pool({
+const pool = new Pool({
     host: process.env.DB_HOST,
-    port:process.env.DB_PORT,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME,
-}
-);
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 async function connectDB(){
     try{
